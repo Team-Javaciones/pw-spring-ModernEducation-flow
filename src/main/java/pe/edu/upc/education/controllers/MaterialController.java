a1e075f72e2a4b9c0f068eab309249a4f1f1ad9d
Renzo <renzodd2001@gmail.com>
2020-10-31 03:00:06 -0500

    added front subir material

---
+++ src/main/java/pe/edu/upc/education/controllers/MaterialController.java
@@ -0,0 +1,50 @@
+package pe.edu.upc.education.controllers;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+import pe.edu.upc.education.models.entities.Material;
+import pe.edu.upc.education.models.entities.Sesion;
+import pe.edu.upc.education.services.MaterialService;
+
+@Controller
+@RequestMapping("/materiales")
+@SessionAttributes("material")
+public class MaterialController {
+	
+	@Autowired
+	private MaterialService materialService;
+	
+	@GetMapping("subir-material")
+	public String crearMaterial(Model model) {
+		Material material = new Material();
+		try {
+			model.addAttribute("material", material);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getMessage());
+		}
+		return "/materiales/subir-material";
+	}
+	
+	@PostMapping("save")	
+	public String save(@ModelAttribute("material") Material material, SessionStatus status) {
+		try {
+			materialService.save(material);
+			status.setComplete();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getMessage());
+		}
+		// Devuelve la URL mapping
+		return "redirect:/materiales/subir-material";
+	}
+}
