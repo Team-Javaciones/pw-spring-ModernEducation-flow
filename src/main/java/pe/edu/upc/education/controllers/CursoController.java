ab51749b4daff0f00b8c522c3774b9b29a9846f9
Diego-Pz <diegopaz25814@gmail.com>
2020-10-31 03:32:11 -0500

    add Visualizar Curso Completo

--- src/main/java/pe/edu/upc/education/controllers/CursoController.java
+++ src/main/java/pe/edu/upc/education/controllers/CursoController.java
@@ -2,12 +2,14 @@ package pe.edu.upc.education.controllers;
 
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
@@ -53,6 +55,20 @@ public class CursoController {
 		return "/cursos/crear-curso";
 	}
 	
+	@GetMapping("view-{id}")
+	public String view(@PathVariable("id") Integer id, Model model) {
+		try {
+			Optional<Curso> optional = cursoService.findById(id);
+			if(optional.isPresent()) {
+				model.addAttribute("curso", optional.get());
+				return "cursos/inicio-cursosView";
+			} 
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getMessage());
+		}
+		return "redirect:/cursos";		
+	}
 	
 	@PostMapping("save")
 	public String save(@ModelAttribute("curso") Curso curso, SessionStatus status) {
@@ -68,6 +84,7 @@ public class CursoController {
 		// Devuelve la URL mapping 
 		return "redirect:/cursos/crear-curso";
 	}
+	
 	@GetMapping("calificar-curso")
 	public String registroAsesor() {		
 		return "/cursos/calificar-curso";
