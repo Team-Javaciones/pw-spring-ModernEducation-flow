785b22ab9489563606226ebfb83d5d9b223cbc57
pakdo20 <generalencasa@gmail.com>
2020-10-31 04:37:13 -0500

    added editar perfil para el usuario

--- src/main/java/pe/edu/upc/education/controllers/AlumnoController.java
+++ src/main/java/pe/edu/upc/education/controllers/AlumnoController.java
@@ -1,12 +1,34 @@
 package pe.edu.upc.education.controllers;
 
+
+
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+
+
+import pe.edu.upc.education.models.entities.Alumno;
+import pe.edu.upc.education.services.AlumnoService;
 
 @Controller
 @RequestMapping("/alumnos")
+@SessionAttributes("alumno")
 public class AlumnoController {
+	
+	@Autowired
+	private AlumnoService alumnoService;
+	
+	
 		
 	@GetMapping("login-alumnos")
 	public String loginAlumno() {		
@@ -18,5 +40,31 @@ public class AlumnoController {
 		return "/alumnos/registro-alumnos";
 	}	
 	
+	@GetMapping("perfil-alumno")
+	public String editarPerifl(Model model) {
+		Alumno alumno = new Alumno();
+		alumno.setId(1);
+		
+		try {
+			model.addAttribute("alumno", alumno);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getMessage());
+		}
+		return "/alumnos/perfil-alumno";
+	}
+	
+	@PostMapping("update")	
+	public String save(@ModelAttribute("alumno") Alumno alumno, SessionStatus status) {
+		try {
+			alumnoService.update(alumno);
+			status.setComplete();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getMessage());
+		}
+		// Devuelve la URL mapping
+		return "redirect:/alumnos/perfil-alumno";
+	}
 }
 
--- src/main/java/pe/edu/upc/education/controllers/AsesorController.java
+++ src/main/java/pe/edu/upc/education/controllers/AsesorController.java
@@ -1,12 +1,26 @@
 package pe.edu.upc.education.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+
+import pe.edu.upc.education.models.entities.Asesor;
+import pe.edu.upc.education.services.AsesorService;
 
 @Controller
 @RequestMapping("/asesores")
+@SessionAttributes("asesor")
 public class AsesorController {	
+	
+	@Autowired
+	private AsesorService asesorService;
 	
 	@GetMapping("login-asesores")
 	public String loginAsesor() {		
@@ -17,4 +31,34 @@ public class AsesorController {
 	public String registroAsesor() {		
 		return "/asesores/registro-asesores";
 	}
+	
+	
+	@GetMapping("perfil-asesor")
+	public String editarPerifl(Model model) {
+		Asesor asesor = new Asesor();
+		asesor.setId(1);
+		
+		
+		try {
+			model.addAttribute("asesor", asesor);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getMessage());
+		}
+		return "/asesores/perfil-asesor";
+	}
+	
+	@PostMapping("update")	
+	public String save(@ModelAttribute("asesor") Asesor asesor, SessionStatus status) {
+		try {
+			asesorService.update(asesor);
+			status.setComplete();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getMessage());
+		}
+		// Devuelve la URL mapping
+		return "redirect:/asesores/perfil-asesor";
+	}
+	
 }
