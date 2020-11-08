1a89c23306408ac1d967611426301a39e888bf09
Diego-Pz <diegopaz25814@gmail.com>
2020-11-08 01:15:33 -0500

    added Menú de Alumno

--- src/main/java/pe/edu/upc/education/controllers/AlumnoController.java
+++ src/main/java/pe/edu/upc/education/controllers/AlumnoController.java
@@ -1,10 +1,5 @@
 package pe.edu.upc.education.controllers;
 
-
-
-
-
-
 import java.util.List;
 import java.util.Optional;
 
@@ -18,41 +13,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
-
-
 import pe.edu.upc.education.models.entities.Alumno;
-
+import pe.edu.upc.education.models.entities.Sesion;
 import pe.edu.upc.education.services.AlumnoService;
 
 @Controller
 @RequestMapping("/alumnos")
 @SessionAttributes("alumno")
 public class AlumnoController {
-	
+
 	@Autowired
 	private AlumnoService alumnoService;
-	
-	
-	
-	
+
 	@GetMapping("registro-alumnos")
-	public String registroAlumno() {		
+	public String registroAlumno() {
 		return "/alumnos/registro-alumnos";
-	}	
-	
-	
-	
-	
-	
+	}
+
 	@GetMapping("perfil-alumno")
 	public String editarPerfil(Model model) {
-		
-		
-		
+
 		try {
 			Optional<Alumno> optional = alumnoService.findById(1);
-			if(optional.isPresent()) {
-			model.addAttribute("alumno", optional.get());
+			if (optional.isPresent()) {
+				model.addAttribute("alumno", optional.get());
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -60,11 +44,11 @@ public class AlumnoController {
 		}
 		return "/alumnos/perfil-alumno";
 	}
-	
-	@PostMapping("update")	
+
+	@PostMapping("update")
 	public String updatePerfil(@ModelAttribute("alumno") Alumno alumno, SessionStatus status) {
 		try {
-			
+
 			alumnoService.update(alumno);
 			status.setComplete();
 		} catch (Exception e) {
@@ -74,31 +58,27 @@ public class AlumnoController {
 		// Devuelve la URL mapping
 		return "redirect:/alumnos/perfil-alumno";
 	}
-	
-	
-	
-	
+
 	@GetMapping("password-alumno")
 	public String editarContra(Model model) {
-			
+
 		try {
 			Optional<Alumno> optional = alumnoService.findById(1);
-			if(optional.isPresent()) {
-			model.addAttribute("alumno", optional.get());
+			if (optional.isPresent()) {
+				model.addAttribute("alumno", optional.get());
 			}
-			} 
-			catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			System.err.println(e.getMessage());
 		}
 		return "/alumnos/password-alumno";
-	
+
 	}
-	
-	@PostMapping("password")	
+
+	@PostMapping("password")
 	public String updateContra(@ModelAttribute("alumno") Alumno alumno, SessionStatus status) {
 		try {
-			
+
 			alumnoService.update(alumno);
 			status.setComplete();
 		} catch (Exception e) {
@@ -108,27 +88,25 @@ public class AlumnoController {
 		// Devuelve la URL mapping
 		return "redirect:/alumnos/password-alumno";
 	}
-	
-	
+
 	@GetMapping("login-alumnos")
 	public String LoginAlumno(Model model) {
 		Alumno alumno = new Alumno();
 		try {
-			model.addAttribute("alumno",alumno);
+			model.addAttribute("alumno", alumno);
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.err.println(e.getMessage());
 		}
 		return "/alumnos/login-alumnos";
 	}
-	
-	
-	@PostMapping("comprobar")	
+
+	@PostMapping("comprobar")
 	public String comprobarAlumno(@ModelAttribute("alumno") Alumno alumno, SessionStatus status) {
 		try {
 			List<Alumno> optionalCorreo = alumnoService.findByCorreoContaining(alumno.getCorreo());
 			List<Alumno> optionalContraseña = alumnoService.findByPasswordContaining(alumno.getPassword());
-			if(!optionalCorreo.isEmpty() && !optionalContraseña.isEmpty()) {
+			if (!optionalCorreo.isEmpty() && !optionalContraseña.isEmpty()) {
 				return "redirect:/inicio-alumnos";
 			}
 		} catch (Exception e) {
@@ -138,8 +116,17 @@ public class AlumnoController {
 		// Devuelve la URL mapping
 		return "redirect:/alumnos/login-alumnos";
 	}
-	
-	
-	
-}
 
+	@GetMapping
+	public String menuAlumno(Model model) {
+		Alumno alumno = new Alumno();
+		try {
+			model.addAttribute("alumno", alumno);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getMessage());
+		}
+		return "/alumnos/menu-alumnos";
+	}
+
+}
